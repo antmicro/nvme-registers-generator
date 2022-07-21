9c45e004c87c871330626af2ecbe2094e678d9a0
Robert Winkler <rwinkler@antmicro.com>
2022-07-21 13:17:08 +0200

    Clean get_reg_map_zephyr.py and add argparse
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- get_reg_map_zephyr.py
+++ get_reg_map_zephyr.py
@@ -3,66 +3,87 @@
 # Copyright (c) 2021 Antmicro <www.antmicro.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-import tabula
-from datetime import datetime
-import subprocess
+import argparse
 import os
+import subprocess
+import tabula
 
-nvme_spec = "NVM-Express-1_4-2019.06.10-Ratified.pdf"
+from datetime import datetime
 
-prefix = "NVME_TC_REG_"
-
-reg_map_fname = "nvme_reg_map.h"
-
-guard = reg_map_fname.replace(".", "_").upper()
-
-reg_width = 4
-
-timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-git_hash = (
-    subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
-    .strip()
-    .decode("ascii")
-)
-fname = os.path.basename(__file__)
-
-table = tabula.read_pdf(
-    nvme_spec, pages="42-43", options="--use-line-returns", lattice=True
-)
+PREFIX = "NVME_TC_REG_"
+PREFACE_TPL = "// Generated on {timestamp} with {file_name}{additional_info}\n\n"
+REG_WIDTH = 4
 
 
 def add_reg(file, name, addr):
-    file.write(f"#define\t{prefix}{name}\t{hex(addr)}\n")
+    file.write(f"#define\t{PREFIX}{name}\t{hex(addr)}\n")
 
 
-with open(reg_map_fname, "w") as reg_map:
-    reg_map.write(f"// Generated on {timestamp} with {fname}, git rev {git_hash}\n")
-    reg_map.write(f"#ifndef {guard}\n#define {guard}\n\n")
-    for line in table[0].iterrows():
-        line = line[1]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate a header file with the registers map for zephyr"
+    )
+    parser.add_argument("input", help="input PDF file with specification")
+    parser.add_argument("output", help="output header file")
+    parser.add_argument(
+        "-f", "--force", action="store_true", help="overwrite output file if exists"
+    )
+    parser.add_argument(
+        "--git-sha",
+        default=None,
+        help="Git sha written to generated file as additional info",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
+    args = parser.parse_args()
 
-        name = line["Unnamed: 1"]
+    if not os.path.isfile(args.input):
+        raise Exception("Input file does not exist")
 
-        if name == "Reserved" or name == "SQ0TDBL":
-            continue
+    if os.path.exists(args.output) and not args.force:
+        raise Exception(f"{args.output} exists. Use -f to overwrite it")
 
-        try:
-            start = int(line["Unnamed: 0"][:-1], base=16)
-            end = int(line["Start"][:-1], base=16)
-            size = end - start + 1
-        except ValueError:
-            continue
+    table = tabula.read_pdf(
+        args.input, pages="42-43", options="--use-line-returns", lattice=True
+    )
 
-        print(f"Register: {name} {hex(start)}-{hex(end)}, {size} bytes")
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    file_name = os.path.basename(args.input)
+    additional_info = ""
+    if args.git_sha is not None:
+        additional_info = ", git_sha {}".format(args.git_sha)
 
-        assert (size % reg_width) == 0
+    preface = PREFACE_TPL.format(
+        timestamp=timestamp, file_name=file_name, additional_info=additional_info
+    )
+    guard = args.input.replace(".", "_").upper()
+    with open(args.output, "w") as reg_map:
+        reg_map.write(preface)
+        reg_map.write(f"#ifndef {guard}\n#define {guard}\n\n")
 
-        if size == reg_width:
-            add_reg(reg_map, name, start)
-        else:
-            for i in range(int(size / reg_width)):
-                add_reg(reg_map, name + "_" + str(i), start + i * reg_width)
+        for line in table[0].iterrows():
+            line = line[1]
+            name = line["Unnamed: 1"]
 
-    reg_map.write("\n#endif\n")
+            if name == "Reserved" or name == "SQ0TDBL":
+                continue
+
+            try:
+                start = int(line["Unnamed: 0"][:-1], base=16)
+                end = int(line["Start"][:-1], base=16)
+                size = end - start + 1
+            except ValueError:
+                continue
+
+            if args.verbose:
+                print(f"Register: {name} {hex(start)}-{hex(end)}, {size} bytes")
+
+            assert (size % REG_WIDTH) == 0
+
+            if size == REG_WIDTH:
+                add_reg(reg_map, name, start)
+            else:
+                for i in range(int(size / REG_WIDTH)):
+                    add_reg(reg_map, name + "_" + str(i), start + i * REG_WIDTH)
+
+        reg_map.write("\n#endif\n")
