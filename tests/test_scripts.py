96158642ddcb00cb5ec2e0bee82a21a183a6d875
Robert Winkler <rwinkler@antmicro.com>
2022-07-21 17:05:33 +0200

    Simplify tests
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -12,25 +12,43 @@ build_dir = os.path.join(root_dir, "test", "build")
 # -- helpers
 
 
-def perform_default_script_operations(script, input, output):
-    cmd = f"./{script} {input} {output}"
-    subprocess.check_call(cmd, shell=True)
+def perform_script_operations(
+    script, input_path, output_name, with_sha=False, with_verbose=False
+):
+    with tempfile.TemporaryDirectory() as output_dir:
+        output_path = os.path.join(output_dir, output_name)
 
-    with pytest.raises(Exception):
-        cmd = f"./{script} {input} {output}"
+        cmd = f"./{script} {input_path} {output_path}"
         subprocess.check_call(cmd, shell=True)
 
-    cmd = f"./{script} -f {input} {output}"
-    subprocess.check_call(cmd, shell=True)
+        with pytest.raises(Exception):
+            cmd = f"./{script} {input_path} {output_path}"
+            subprocess.check_call(cmd, shell=True)
 
-    with pytest.raises(Exception):
-        cmd = f"./{script} -v non-existing-file-path {output}"
+        cmd = f"./{script} -f {input_path} {output_path}"
         subprocess.check_call(cmd, shell=True)
 
+        with pytest.raises(Exception):
+            cmd = f"./{script} -v non-existing-file-path {output_path}"
+            subprocess.check_call(cmd, shell=True)
 
-def generate_register_json(input, output, force=False):
+    if with_sha:
+        with tempfile.TemporaryDirectory() as output_dir:
+            output_path = os.path.join(output_dir, output_name)
+            git_sha = "71cabda"
+            cmd = f"./{script} --git-sha={git_sha} {input_path} {output_path}"
+            subprocess.check_call(cmd, shell=True)
+
+    if with_verbose:
+        with tempfile.TemporaryDirectory() as output_dir:
+            output_path = os.path.join(output_dir, output_name)
+            cmd = f"./{script} -v {nvme_spec_path} {output_path}"
+            subprocess.check_call(cmd, shell=True)
+
+
+def generate_register_json(input_path, output_path, force=False):
     args = "-f" if force else ""
-    cmd = f"./get_reg_fields.py {args} {input} {output}"
+    cmd = f"./get_reg_fields.py {args} {input_path} {output_path}"
     subprocess.check_call(cmd, shell=True)
 
 
@@ -39,114 +57,57 @@ def generate_register_json(input, output, force=False):
 
 def test_get_reg_fields():
     script = "get_reg_fields.py"
-    output_file = "registers.json"
+    output_name = "registers.json"
 
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        perform_default_script_operations(script, nvme_spec_path, output_path)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        cmd = f"./{script} -v {nvme_spec_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
+    perform_script_operations(script, nvme_spec_path, output_name, with_verbose=True)
 
 
 def test_get_reg_fields_chisel():
     script = "get_reg_fields_chisel.py"
-    output_file = "RegisterDefs.scala"
-    register_json_file = "registers.json"
+    output_name = "RegisterDefs.scala"
+    register_json_name = "registers.json"
 
     with tempfile.TemporaryDirectory() as output_dir:
-        register_json_path = os.path.join(output_dir, register_json_file)
+        register_json_path = os.path.join(output_dir, register_json_name)
         generate_register_json(nvme_spec_path, register_json_path)
-
-        output_path = os.path.join(output_dir, output_file)
-        perform_default_script_operations(script, register_json_path, output_path)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        register_json_path = os.path.join(output_dir, register_json_file)
-        generate_register_json(nvme_spec_path, register_json_path)
-        output_path = os.path.join(output_dir, output_file)
-
-        git_sha = "71cabda"
-        cmd = f"./{script} --git-sha={git_sha} {register_json_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
+        perform_script_operations(
+            script, register_json_path, output_name, with_sha=True
+        )
 
 
 def test_get_reg_map_chisel():
     script = "get_reg_map_chisel.py"
-    output_file = "CSRRegMap.scala"
+    output_name = "CSRRegMap.scala"
 
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        perform_default_script_operations(script, nvme_spec_path, output_path)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        cmd = f"./{script} -v {nvme_spec_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        git_sha = "71cabda"
-        cmd = f"./{script} --git-sha={git_sha} {nvme_spec_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
+    perform_script_operations(
+        script, nvme_spec_path, output_name, with_sha=True, with_verbose=True
+    )
 
 
 def test_get_reg_fields_zephyr():
     script = "get_reg_fields_zephyr.py"
-    output_file = "nvme_reg_fields.h"
-    register_json_file = "registers.json"
+    output_name = "nvme_reg_fields.h"
+    register_json_name = "registers.json"
 
     with tempfile.TemporaryDirectory() as output_dir:
-        register_json_path = os.path.join(output_dir, register_json_file)
+        register_json_path = os.path.join(output_dir, register_json_name)
         generate_register_json(nvme_spec_path, register_json_path)
-
-        output_path = os.path.join(output_dir, output_file)
-        perform_default_script_operations(script, register_json_path, output_path)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        register_json_path = os.path.join(output_dir, register_json_file)
-        generate_register_json(nvme_spec_path, register_json_path)
-        output_path = os.path.join(output_dir, output_file)
-
-        git_sha = "71cabda"
-        cmd = f"./{script} --git-sha={git_sha} {register_json_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
+        perform_script_operations(
+            script, register_json_path, output_name, with_sha=True
+        )
 
 
 def test_get_identify_struct():
     script = "get_identify_struct.py"
-    output_file = "nvme_ident_fields.h"
+    output_name = "nvme_ident_fields.h"
 
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        perform_default_script_operations(script, nvme_spec_path, output_path)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-
-        git_sha = "71cabda"
-        cmd = f"./{script} --git-sha={git_sha} {nvme_spec_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
+    perform_script_operations(script, nvme_spec_path, output_name, with_sha=True)
 
 
 def test_get_reg_map_zephyr():
     script = "get_reg_map_zephyr.py"
-    output_file = "nvme_reg_map.h"
+    output_name = "nvme_reg_map.h"
 
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        perform_default_script_operations(script, nvme_spec_path, output_path)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-        cmd = f"./{script} -v {nvme_spec_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
-
-    with tempfile.TemporaryDirectory() as output_dir:
-        output_path = os.path.join(output_dir, output_file)
-
-        git_sha = "71cabda"
-        cmd = f"./{script} --git-sha={git_sha} {nvme_spec_path} {output_path}"
-        subprocess.check_call(cmd, shell=True)
+    perform_script_operations(
+        script, nvme_spec_path, output_name, with_sha=True, with_verbose=True
+    )
